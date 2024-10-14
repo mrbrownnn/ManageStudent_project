766fc8c783d1188a39e697cb2bfcdc6ccdec7651
Pham Thanh <102653760+mrbrownnn@users.noreply.github.com>
2024-10-15 06:18:37 +0700

    fix conflict

--- src/main/java/com/fpt/manage/Springboot_Manage_app/repository/RoleRepository_BACKUP_1707.java
+++ src/main/java/com/fpt/manage/Springboot_Manage_app/repository/RoleRepository_BACKUP_1707.java
@@ -2,24 +2,17 @@ package com.fpt.manage.Springboot_Manage_app.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.*;
-import java.util.Optional;
-import com.fpt.manage.Springboot_Manage_app.domain.entities.Role;
-import com.fpt.manage.Springboot_Manage_app.domain.entities.RoleName;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+import java.util.Optional;
+import com.fpt.manage.Springboot_Manage_app.domain.entities.Accounts;
+import com.fpt.manage.Springboot_Manage_app.domain.entities.RoleName;
+
+public interface RoleRepository extends JpaRepository<Accounts, Long> {
 <<<<<<< HEAD
-    Optional<Role> findByName(RoleName roleName);
+    Optional<Accounts> findByName(RoleName roleName);
 }
 // need class include RoleName and Optinal<Role>
 =======
-    Optional<Role> findByName(RoleName name);
+    Optional<Accounts> findByName(RoleName name);
 }
 >>>>>>> 60ad27914cbaea40a5bd1d2e0d19ab18a38acb8c
--- src/main/java/com/fpt/manage/Springboot_Manage_app/repository/RoleRepository_REMOTE_1707.java
+++ src/main/java/com/fpt/manage/Springboot_Manage_app/repository/RoleRepository_REMOTE_1707.java
@@ -3,18 +3,11 @@ package com.fpt.manage.Springboot_Manage_app.repository;
 
 import com.fpt.manage.Springboot_Manage_app.domain.entrities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.*;
-import java.util.Optional;
-import com.fpt.manage.Springboot_Manage_app.domain.entities.Role;
-import com.fpt.manage.Springboot_Manage_app.domain.entities.RoleName;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(RoleName name);
+import java.util.Optional;
+import com.fpt.manage.Springboot_Manage_app.domain.entities.Accounts;
+import com.fpt.manage.Springboot_Manage_app.domain.entities.RoleName;
+
+public interface RoleRepository extends JpaRepository<Accounts, Long> {
+    Optional<Accounts> findByName(RoleName name);
 }
