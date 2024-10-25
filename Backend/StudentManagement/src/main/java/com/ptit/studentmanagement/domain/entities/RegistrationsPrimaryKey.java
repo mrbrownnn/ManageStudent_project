9ba6fc776f2eefe72fd7141a79fac7dcdaf4cd3f
Pham Thanh <102653760+mrbrownnn@users.noreply.github.com>
2024-10-25 11:54:22 +0700

    Rebase toàn bộ database 😭😭😭

--- Backend/StudentManagement/src/main/java/com/ptit/studentmanagement/domain/entities/RegistrationsPrimaryKey.java
+++
@@ -1,8 +0,0 @@
-package com.ptit.studentmanagement.domain.entities;
-
-import java.io.Serializable;
-
-public class RegistrationsPrimaryKey implements Serializable {
-    private String studentId;
-    private String classId;
-}
